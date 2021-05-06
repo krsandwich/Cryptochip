4393a6457ce86ed1977584486a58c16c6f8b0770
krsandwich <krzanich@stanford.edu>
2021-05-06 00:07:57 -0700

    trying new multiply

--- AES/design/rtl/AESTopTest.v
+++ AES/design/rtl/AESTopTest.v
@@ -137,11 +137,11 @@ module AESEncrypt
   // INPUT OUTPUT 
   // - this is for storing input and output data 
   //---------
-  reg [ 127: 0] temp_data  [5:0];
+  reg [ 127: 0] temp_data  [30:0];
   reg [   0: 0] temp_ready;
   reg [   0: 0] valid_final;
-  reg [ 127: 0] temp_key;
-  reg [   1: 0] state;
+  reg [ 127: 0] data_final;
+  reg [   0: 0] state;
   reg [   3: 0] counter;
   localparam WAIT = 2'd0, ENCRYPT = 2'd1, FINAL = 2'd2;
 
@@ -149,22 +149,28 @@ module AESEncrypt
   EncryptInitRound  Round0 (.data_in(temp_data[0]), .key(key[0]),
                             .data_out(temp_data[1]));
 
-  EncryptRound      EncryptRound (.data_in(temp_data[2]), .key(temp_key),
-                                  .data_out(temp_data[3]));
+  genvar i; 
+  generate
+    for (i=0; i<13; i+=1) begin : EncryptRounds 
+      EncryptRound  EncryptRound (.data_in(temp_data[(i<<1) + 2]), .key(key[i+1]),
+                                  .data_out(temp_data[(i<<1) + 3]));
 
+  end 
+  endgenerate
 
-  EncryptLastRound  Round14 (.data_in(temp_data[4]), .key(key[14]),
-                             .data_out(temp_data[5]));
+  EncryptLastRound  Round14 (.data_in(temp_data[28]), .key(key[14]),
+                             .data_out(temp_data[29]));
 
 
   //---------
   // FSM
   //---------
-  assign data_out = temp_data[5];
+  assign data_out = temp_data[29];
   assign valid = valid_final;
 
   always_ff @(posedge clk or posedge rst) begin
     if (rst) begin
+      data_final <= 128'd0;
       valid_final <= 1'b0; 
       state <= WAIT;
     end
@@ -180,14 +186,23 @@ module AESEncrypt
         end
         ENCRYPT: begin
           counter <= counter + 4'd1;
-          if(counter == 4'd1) begin
-            temp_data[2] <= temp_data[1];
-            temp_key <= key[1];
-          end else if(counter < 4'd14) begin
-            temp_data[2] <= temp_data[3];
-            temp_key <= key[counter];
-          end else if(counter == 4'd14) begin
-            temp_data[4] <= temp_data[3];
+          case(counter)
+            4'd1: temp_data[2] <= temp_data[1];
+            4'd2: temp_data[4] <= temp_data[3];
+            4'd3: temp_data[6] <= temp_data[5];
+            4'd4: temp_data[8] <= temp_data[7];
+            4'd5: temp_data[10] <= temp_data[9];
+            4'd6: temp_data[12] <= temp_data[11];
+            4'd7: temp_data[14] <= temp_data[13];
+            4'd8: temp_data[16] <= temp_data[15];
+            4'd9: temp_data[18] <= temp_data[17];
+            4'd10: temp_data[20] <= temp_data[19];
+            4'd11: temp_data[22] <= temp_data[21];
+            4'd12: temp_data[24] <= temp_data[23];
+            4'd13: temp_data[26] <= temp_data[25];
+            4'd14: temp_data[28] <= temp_data[27];
+          endcase
+          if(counter == 4'd14) begin
             state <= FINAL;
             valid_final <= 1'd1;
           end
@@ -225,29 +240,34 @@ module AESDecrypt
   // INPUT OUTPUT 
   // - this is for storing input and output data 
   //---------
-  reg [ 127: 0] temp_data  [5:0];
+  reg [ 127: 0] temp_data  [30:0];
   reg [   0: 0] temp_ready;
   reg [   0: 0] valid_final;
-  reg [ 127: 0] temp_key;
-  reg [   1: 0] state;
+  reg [ 127: 0] data_final;
+  reg [   0: 0] state;
   reg [   3: 0] counter;
   localparam WAIT = 2'd0, DECRYPT = 2'd1, FINAL = 2'd2;
 
   DecryptInitRound  Round0 (.data_in(temp_data[0]), .key(key[14]),
                             .data_out(temp_data[1]));
 
-  DecryptRound  DecryptRound (.data_in(temp_data[2]), .key(temp_key),
-                              .data_out(temp_data[3]));
+  genvar i;
+  generate
+    for (i=0; i<13; i+=1) begin : DecryptRounds 
+      DecryptRound  DecryptRound (.data_in(temp_data[(i<<1) + 2]), .key(key[13-i]),
+                                  .data_out(temp_data[(i<<1) + 3]));
+    end 
+  endgenerate
 
-  DecryptLastRound  Round14 (.data_in(temp_data[4]), .key(key[0]),
-                             .data_out(temp_data[5]));
+  DecryptLastRound  Round14 (.data_in(temp_data[28]), .key(key[0]),
+                             .data_out(temp_data[29]));
 
 
 
   //---------
   // FSM
   //---------
-  assign data_out = temp_data[5];
+  assign data_out = temp_data[29];
   assign valid = valid_final;
 
     always_ff @(posedge clk or posedge rst) begin
@@ -267,14 +287,23 @@ module AESDecrypt
         end
         DECRYPT: begin
           counter <= counter + 4'd1;
-          if(counter == 4'd1) begin
-            temp_data[2] <= temp_data[1];
-            temp_key <= key[4'd13];
-          end else if(counter < 4'd14) begin
-            temp_data[2] <= temp_data[3];
-            temp_key <= key[4'd14 - counter];
-          end else if(counter == 4'd14) begin
-            temp_data[4] <= temp_data[3];
+          case(counter)
+            4'd1: temp_data[2] <= temp_data[1];
+            4'd2: temp_data[4] <= temp_data[3];
+            4'd3: temp_data[6] <= temp_data[5];
+            4'd4: temp_data[8] <= temp_data[7];
+            4'd5: temp_data[10] <= temp_data[9];
+            4'd6: temp_data[12] <= temp_data[11];
+            4'd7: temp_data[14] <= temp_data[13];
+            4'd8: temp_data[16] <= temp_data[15];
+            4'd9: temp_data[18] <= temp_data[17];
+            4'd10: temp_data[20] <= temp_data[19];
+            4'd11: temp_data[22] <= temp_data[21];
+            4'd12: temp_data[24] <= temp_data[23];
+            4'd13: temp_data[26] <= temp_data[25];
+            4'd14: temp_data[28] <= temp_data[27];
+          endcase
+          if(counter == 4'd14) begin
             state <= FINAL;
             valid_final <= 1'd1;
           end
@@ -439,20 +468,19 @@ module ExpandKey
   output wire [    0:0] valid
 );
 
-  wire [127:0] temp_key [1:0];
-
+  wire [127:0] temp_key [14:0];
   reg  [127:0] temp_out [14:0];
-  reg  [255:0] next_key [3:0]; 
+  reg  [255:0] next_key [25:0]; 
   reg  [  0:0] temp_ready;
   reg  [  0:0] valid_final;
-  reg  [  1:0] state;
+  reg  [  0:0] state;
   reg  [  3:0] counter;
-  reg  [  7:0] temp_rcon;
+  reg  [  5:0] index;
   localparam WAIT = 2'd0, EXPAND = 2'd1, FINAL = 2'd2;
   //---------
   // FSM
   //---------
-  assign key_out = temp_out;
+  assign key_out = temp_key;
   assign valid = valid_final;
 
   always_ff @(posedge clk or posedge rst) begin
@@ -467,43 +495,32 @@ module ExpandKey
         WAIT: begin
           if (ready) begin
             state <= EXPAND;
-            temp_out[0] <= key_in[255:128];
-            temp_out[1] <= key_in[127:0]; 
             next_key[0] <= key_in;
-            temp_rcon <= 8'h1;
+            index = 6'd2;
           end 
           valid_final <= 1'd0;
-          counter <= 4'd2;
+          counter <= 4'd1;
         end
         EXPAND: begin
           counter <= counter + 4'd1;
-          if(counter[0]) begin
-            temp_out[counter + 4'd0] <= temp_key[1];
-            next_key[0] <= next_key[3];
-          end else begin 
-            temp_out[counter + 4'd0] <= temp_key[0];
-            next_key[2] <= next_key[1];
-          end
-          if(counter == 4'd14) begin
+          case(counter)
+            4'd1: next_key[2] <= next_key[1];
+            4'd2: next_key[4] <= next_key[3];
+            4'd3: next_key[6] <= next_key[5];
+            4'd4: next_key[8] <= next_key[7];
+            4'd5: next_key[10] <= next_key[9];
+            4'd6: next_key[12] <= next_key[11];
+            4'd7: next_key[14] <= next_key[13];
+            4'd8: next_key[16] <= next_key[15];
+            4'd9: next_key[18] <= next_key[17];
+            4'd10: next_key[20] <= next_key[19];
+            4'd11: next_key[22] <= next_key[21];
+            4'd12: next_key[24] <= next_key[23];
+          endcase
+          if(counter == 4'd12) begin
             state <= FINAL;
             valid_final <= 1'd1;
           end
-          case(counter)
-            4'd1: temp_rcon <= 8'h1;
-            4'd2: temp_rcon <= 8'h1;
-            4'd3: temp_rcon <= 8'h2;
-            4'd4: temp_rcon <= 8'h2;
-            4'd5: temp_rcon <= 8'h4;
-            4'd6: temp_rcon <= 8'h4;
-            4'd7: temp_rcon <= 8'h8;
-            4'd8: temp_rcon <= 8'h8;
-            4'd9: temp_rcon <= 8'h10;
-            4'd10: temp_rcon <= 8'h10;
-            4'd11: temp_rcon <= 8'h20;
-            4'd12: temp_rcon <= 8'h20;
-            4'd13: temp_rcon <= 8'h40;
-            4'd14: temp_rcon <= 8'h40;
-          endcase
         end
         FINAL: begin
           valid_final <= 1'd0;
@@ -513,9 +530,22 @@ module ExpandKey
     end
   end
 
+  assign temp_key[0] = key_in[255:128];
+  assign temp_key[1] = key_in[127:0]; 
 
-  ExpandKeyEvenHelper round2  (.in(next_key[0]), .rcon(temp_rcon), .next(next_key[1]), .out(temp_key[0]));
-  ExpandKeyOddHelper  round3  (.in(next_key[2]), .next(next_key[3]), .out(temp_key[1]));
+  ExpandKeyEvenHelper round2  (.in(next_key[0]), .rcon(8'h1), .next(next_key[1]), .out(temp_key[2]));
+  ExpandKeyOddHelper  round3  (.in(next_key[2]), .next(next_key[3]), .out(temp_key[3]));
+  ExpandKeyEvenHelper round4  (.in(next_key[4]), .rcon(8'h2), .next(next_key[5]), .out(temp_key[4]));
+  ExpandKeyOddHelper  round5  (.in(next_key[6]), .next(next_key[7]), .out(temp_key[5]));
+  ExpandKeyEvenHelper round6  (.in(next_key[8]), .rcon(8'h4), .next(next_key[9]), .out(temp_key[6]));
+  ExpandKeyOddHelper  round7  (.in(next_key[10]), .next(next_key[11]), .out(temp_key[7]));
+  ExpandKeyEvenHelper round8  (.in(next_key[12]), .rcon(8'h8), .next(next_key[13]), .out(temp_key[8]));
+  ExpandKeyOddHelper  round9  (.in(next_key[14]), .next(next_key[15]), .out(temp_key[9]));
+  ExpandKeyEvenHelper round10 (.in(next_key[16]), .rcon(8'h10), .next(next_key[17]), .out(temp_key[10]));
+  ExpandKeyOddHelper  round11 (.in(next_key[18]), .next(next_key[19]), .out(temp_key[11]));
+  ExpandKeyEvenHelper round12 (.in(next_key[20]), .rcon(8'h20), .next(next_key[21]), .out(temp_key[12]));
+  ExpandKeyOddHelper  round13 (.in(next_key[22]), .next(next_key[23]), .out(temp_key[13]));
+  ExpandKeyEvenHelper round14 (.in(next_key[24]), .rcon(8'h40), .next( ), .out(temp_key[14]));
 
 endmodule
 
@@ -628,21 +658,276 @@ xtimes xt_u (.in(in), .out(xt));
 assign out = xt ^ in;
 
 endmodule
+// 10 = a, 11 = b, 12 = c, 13 = d, 14 = e
 
 module MULE (
   input wire [7:0] in,
   output wire [7:0] out
 );
- 
- 
-wire [7:0] xt1, xt2, xt3;
-
-xtimes xt_u1 (.in(in), .out(xt1));
-xtimes xt_u2 (.in(xt1), .out(xt2));
-xtimes xt_u3 (.in(xt2), .out(xt3));
-
-assign out = xt3 ^ xt2 ^ xt1;
-
+    
+  reg [7:0] out_temp;
+  assign out = out_temp;
+    
+  always @(in)
+    case (in)
+      8'h00: out_temp = 8'h00;
+      8'h01: out_temp = 8'h0e;
+      8'h02: out_temp = 8'h1c;
+      8'h03: out_temp = 8'h12;
+      8'h04: out_temp = 8'h38;
+      8'h05: out_temp = 8'h36;
+      8'h06: out_temp = 8'h24;
+      8'h07: out_temp = 8'h2a;
+      8'h08: out_temp = 8'h70;
+      8'h09: out_temp = 8'h7e;
+      8'h0a: out_temp = 8'h6c;
+      8'h0b: out_temp = 8'h62;
+      8'h0c: out_temp = 8'h48;
+      8'h0d: out_temp = 8'h46;
+      8'h0e: out_temp = 8'h54;
+      8'h0f: out_temp = 8'h5a;
+      8'h10: out_temp = 8'he0;
+      8'h11: out_temp = 8'hee;
+      8'h12: out_temp = 8'hfc;
+      8'h13: out_temp = 8'hf2;
+      8'h14: out_temp = 8'hd8;
+      8'h15: out_temp = 8'hd6;
+      8'h16: out_temp = 8'hc4;
+      8'h17: out_temp = 8'hca;
+      8'h18: out_temp = 8'h90;
+      8'h19: out_temp = 8'h9e;
+      8'h1a: out_temp = 8'h8c;
+      8'h1b: out_temp = 8'h82;
+      8'h1c: out_temp = 8'ha8;
+      8'h1d: out_temp = 8'ha6;
+      8'h1e: out_temp = 8'hb4;
+      8'h1f: out_temp = 8'hba;
+      8'h20: out_temp = 8'hdb;
+      8'h21: out_temp = 8'hd5;
+      8'h22: out_temp = 8'hc7;
+      8'h23: out_temp = 8'hc9;
+      8'h24: out_temp = 8'he3;
+      8'h25: out_temp = 8'hed;
+      8'h26: out_temp = 8'hff;
+      8'h27: out_temp = 8'hf1;
+      8'h28: out_temp = 8'hab;
+      8'h29: out_temp = 8'ha5;
+      8'h2a: out_temp = 8'hb7;
+      8'h2b: out_temp = 8'hb9;
+      8'h2c: out_temp = 8'h93;
+      8'h2d: out_temp = 8'h9d;
+      8'h2e: out_temp = 8'h8f;
+      8'h2f: out_temp = 8'h81;
+      8'h30: out_temp = 8'h3b;
+      8'h31: out_temp = 8'h35;
+      8'h32: out_temp = 8'h27;
+      8'h33: out_temp = 8'h29;
+      8'h34: out_temp = 8'h03;
+      8'h35: out_temp = 8'h0d;
+      8'h36: out_temp = 8'h1f;
+      8'h37: out_temp = 8'h11;
+      8'h38: out_temp = 8'h4b;
+      8'h39: out_temp = 8'h45;
+      8'h3a: out_temp = 8'h57;
+      8'h3b: out_temp = 8'h59;
+      8'h3c: out_temp = 8'h73;
+      8'h3d: out_temp = 8'h7d;
+      8'h3e: out_temp = 8'h6f;
+      8'h3f: out_temp = 8'h61;
+      8'h40: out_temp = 8'had;
+      8'h41: out_temp = 8'ha3;
+      8'h42: out_temp = 8'hb1;
+      8'h43: out_temp = 8'hbf;
+      8'h44: out_temp = 8'h95;
+      8'h45: out_temp = 8'h9b;
+      8'h46: out_temp = 8'h89;
+      8'h47: out_temp = 8'h87;
+      8'h48: out_temp = 8'hdd;
+      8'h49: out_temp = 8'hd3;
+      8'h4a: out_temp = 8'hc1;
+      8'h4b: out_temp = 8'hcf;
+      8'h4c: out_temp = 8'he5;
+      8'h4d: out_temp = 8'heb;
+      8'h4e: out_temp = 8'hf9;
+      8'h4f: out_temp = 8'hf7;
+      8'h50: out_temp = 8'h4d;
+      8'h51: out_temp = 8'h43;
+      8'h52: out_temp = 8'h51;
+      8'h53: out_temp = 8'h5f;
+      8'h54: out_temp = 8'h75;
+      8'h55: out_temp = 8'h7b;
+      8'h56: out_temp = 8'h69;
+      8'h57: out_temp = 8'h67;
+      8'h58: out_temp = 8'h3d;
+      8'h59: out_temp = 8'h33;
+      8'h5a: out_temp = 8'h21;
+      8'h5b: out_temp = 8'h2f;
+      8'h5c: out_temp = 8'h05;
+      8'h5d: out_temp = 8'h0b;
+      8'h5e: out_temp = 8'h19;
+      8'h5f: out_temp = 8'h17;
+      8'h60: out_temp = 8'h76;
+      8'h61: out_temp = 8'h78;
+      8'h62: out_temp = 8'h6a;
+      8'h63: out_temp = 8'h64;
+      8'h64: out_temp = 8'h4e;
+      8'h65: out_temp = 8'h40;
+      8'h66: out_temp = 8'h52;
+      8'h67: out_temp = 8'h5c;
+      8'h68: out_temp = 8'h06;
+      8'h69: out_temp = 8'h08;
+      8'h6a: out_temp = 8'h1a;
+      8'h6b: out_temp = 8'h14;
+      8'h6c: out_temp = 8'h3e;
+      8'h6d: out_temp = 8'h30;
+      8'h6e: out_temp = 8'h22;
+      8'h6f: out_temp = 8'h2c;
+      8'h70: out_temp = 8'h96;
+      8'h71: out_temp = 8'h98;
+      8'h72: out_temp = 8'h8a;
+      8'h73: out_temp = 8'h84;
+      8'h74: out_temp = 8'hae;
+      8'h75: out_temp = 8'ha0;
+      8'h76: out_temp = 8'hb2;
+      8'h77: out_temp = 8'hbc;
+      8'h78: out_temp = 8'he6;
+      8'h79: out_temp = 8'he8;
+      8'h7a: out_temp = 8'hfa;
+      8'h7b: out_temp = 8'hf4;
+      8'h7c: out_temp = 8'hde;
+      8'h7d: out_temp = 8'hd0;
+      8'h7e: out_temp = 8'hc2;
+      8'h7f: out_temp = 8'hcc;
+      8'h80: out_temp = 8'h41;
+      8'h81: out_temp = 8'h4f;
+      8'h82: out_temp = 8'h5d;
+      8'h83: out_temp = 8'h53;
+      8'h84: out_temp = 8'h79;
+      8'h85: out_temp = 8'h77;
+      8'h86: out_temp = 8'h65;
+      8'h87: out_temp = 8'h6b;
+      8'h88: out_temp = 8'h31;
+      8'h89: out_temp = 8'h3f;
+      8'h8a: out_temp = 8'h2d;
+      8'h8b: out_temp = 8'h23;
+      8'h8c: out_temp = 8'h09;
+      8'h8d: out_temp = 8'h07;
+      8'h8e: out_temp = 8'h15;
+      8'h8f: out_temp = 8'h1b;
+      8'h90: out_temp = 8'ha1;
+      8'h91: out_temp = 8'haf;
+      8'h92: out_temp = 8'hbd;
+      8'h93: out_temp = 8'hb3;
+      8'h94: out_temp = 8'h99;
+      8'h95: out_temp = 8'h97;
+      8'h96: out_temp = 8'h85;
+      8'h97: out_temp = 8'h8b;
+      8'h98: out_temp = 8'hd1;
+      8'h99: out_temp = 8'hdf;
+      8'h9a: out_temp = 8'hcd;
+      8'h9b: out_temp = 8'hc3;
+      8'h9c: out_temp = 8'he9;
+      8'h9d: out_temp = 8'he7;
+      8'h9e: out_temp = 8'hf5;
+      8'h9f: out_temp = 8'hfb;
+      8'ha0: out_temp = 8'h9a;
+      8'ha1: out_temp = 8'h94;
+      8'ha2: out_temp = 8'h86;
+      8'ha3: out_temp = 8'h88;
+      8'ha4: out_temp = 8'ha2;
+      8'ha5: out_temp = 8'hac;
+      8'ha6: out_temp = 8'hbe;
+      8'ha7: out_temp = 8'hb0;
+      8'ha8: out_temp = 8'hea;
+      8'ha9: out_temp = 8'he4;
+      8'haa: out_temp = 8'hf6;
+      8'hab: out_temp = 8'hf8;
+      8'hac: out_temp = 8'hd2;
+      8'had: out_temp = 8'hdc;
+      8'hae: out_temp = 8'hce;
+      8'haf: out_temp = 8'hc0;
+      8'hb0: out_temp = 8'h7a;
+      8'hb1: out_temp = 8'h74;
+      8'hb2: out_temp = 8'h66;
+      8'hb3: out_temp = 8'h68;
+      8'hb4: out_temp = 8'h42;
+      8'hb5: out_temp = 8'h4c;
+      8'hb6: out_temp = 8'h5e;
+      8'hb7: out_temp = 8'h50;
+      8'hb8: out_temp = 8'h0a;
+      8'hb9: out_temp = 8'h04;
+      8'hba: out_temp = 8'h16;
+      8'hbb: out_temp = 8'h18;
+      8'hbc: out_temp = 8'h32;
+      8'hbd: out_temp = 8'h3c;
+      8'hbe: out_temp = 8'h2e;
+      8'hbf: out_temp = 8'h20;
+      8'hc0: out_temp = 8'hec;
+      8'hc1: out_temp = 8'he2;
+      8'hc2: out_temp = 8'hf0;
+      8'hc3: out_temp = 8'hfe;
+      8'hc4: out_temp = 8'hd4;
+      8'hc5: out_temp = 8'hda;
+      8'hc6: out_temp = 8'hc8;
+      8'hc7: out_temp = 8'hc6;
+      8'hc8: out_temp = 8'h9c;
+      8'hc9: out_temp = 8'h92;
+      8'hca: out_temp = 8'h80;
+      8'hcb: out_temp = 8'h8e;
+      8'hcc: out_temp = 8'ha4;
+      8'hcd: out_temp = 8'haa;
+      8'hce: out_temp = 8'hb8;
+      8'hcf: out_temp = 8'hb6;
+      8'hd0: out_temp = 8'h0c;
+      8'hd1: out_temp = 8'h02;
+      8'hd2: out_temp = 8'h10;
+      8'hd3: out_temp = 8'h1e;
+      8'hd4: out_temp = 8'h34;
+      8'hd5: out_temp = 8'h3a;
+      8'hd6: out_temp = 8'h28;
+      8'hd7: out_temp = 8'h26;
+      8'hd8: out_temp = 8'h7c;
+      8'hd9: out_temp = 8'h72;
+      8'hda: out_temp = 8'h60;
+      8'hdb: out_temp = 8'h6e;
+      8'hdc: out_temp = 8'h44;
+      8'hdd: out_temp = 8'h4a;
+      8'hde: out_temp = 8'h58;
+      8'hdf: out_temp = 8'h56;
+      8'he0: out_temp = 8'h37;
+      8'he1: out_temp = 8'h39;
+      8'he2: out_temp = 8'h2b;
+      8'he3: out_temp = 8'h25;
+      8'he4: out_temp = 8'h0f;
+      8'he5: out_temp = 8'h01;
+      8'he6: out_temp = 8'h13;
+      8'he7: out_temp = 8'h1d;
+      8'he8: out_temp = 8'h47;
+      8'he9: out_temp = 8'h49;
+      8'hea: out_temp = 8'h5b;
+      8'heb: out_temp = 8'h55;
+      8'hec: out_temp = 8'h7f;
+      8'hed: out_temp = 8'h71;
+      8'hee: out_temp = 8'h63;
+      8'hef: out_temp = 8'h6d;
+      8'hf0: out_temp = 8'hd7;
+      8'hf1: out_temp = 8'hd9;
+      8'hf2: out_temp = 8'hcb;
+      8'hf3: out_temp = 8'hc5;
+      8'hf4: out_temp = 8'hef;
+      8'hf5: out_temp = 8'he1;
+      8'hf6: out_temp = 8'hf3;
+      8'hf7: out_temp = 8'hfd;
+      8'hf8: out_temp = 8'ha7;
+      8'hf9: out_temp = 8'ha9;
+      8'hfa: out_temp = 8'hbb;
+      8'hfb: out_temp = 8'hb5;
+      8'hfc: out_temp = 8'h9f;
+      8'hfd: out_temp = 8'h91;
+      8'hfe: out_temp = 8'h83;
+      8'hff: out_temp = 8'h8d;
+      default: out_temp = 8'h00;
+  endcase
 endmodule
 
 module MULB (
